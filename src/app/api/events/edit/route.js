eb8625ba3e03b6e0aca3c48c7301a6bbb1d8e3b2
EiichiM <matsuyoubi@gmail.com>
2023-08-02 19:35:02 -0600

    put correction

--- src/app/api/events/edit/route.js
+++ src/app/api/events/edit/route.js
@@ -3,7 +3,7 @@ import Event from "@/models/eventModel";
 import { connectDB } from "@/config/dbConfig";
 connectDB();
 
-export async function GET(request) {
+export async function PUT(request) {
   try {
     const { eventName, eventType, imageBanner, clients } = await request.json();
     const eventid = request.url.split("events/")[1];
